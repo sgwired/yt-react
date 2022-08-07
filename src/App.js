df6560990517aeb47776dd4cb17604828c0121a9
Shelton Gayles <sg.bythelake@gmail.com>
2022-08-06 18:35:47 -0700

    update item

--- src/App.js
+++ src/App.js
@@ -52,16 +52,30 @@ function App() {
     if(result) setFetchError(result);
   };
 
-  const handleCheck = (id) => {
+  const handleCheck =  async (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
     setItems(listItems);
+
+    const myItem = listItems.filter((item) => item.id === id)
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({checked: myItem[0].checked})
+    }
+    const reqUrl = `${API_URL}/${id}`
+    const result = await apiRequest(reqUrl, updateOptions);
+    if(result) setFetchError(result);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
     setItems(listItems);
+
+    
   };
 
   const handleSubmit = (e) => {
